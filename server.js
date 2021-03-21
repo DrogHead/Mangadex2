5abe4102278e80c278122f6fefed39c89c18a584
DrogHead <giuvit003@gmail.com>
2021-03-22 00:51:55 +0100

    itnowseemstowork

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = 8000;
 const dirPath = path.join(__dirname, 'public/images/Maou');
 
 const maouLinks = [];
+const maouNums = [];
 
 var nMaou = 0;
 fs.readdir(dirPath, (err, files) => {
@@ -17,9 +18,23 @@ fs.readdir(dirPath, (err, files) => {
     files.forEach(file => {
         nMaou++;
         let numLink = (nMaou < 100 ? (nMaou < 10 ? "00" : "0") : "") + nMaou;
-        maouLinks.push("http://127.0.0.1:8000/cap" + numLink);
+
+        maouLinks.push("http://127.0.0.1:8000/Maou/cap" + numLink);
+
+        fillNums(path.join(dirPath, 'C'+numLink));
+
     });
 });
+
+function fillNums(pth) {
+    let n = 0;
+    let files = fs.readdirSync(pth);
+    files.forEach(file => {
+        n++;
+    });
+    maouNums.push(n);
+}
+
 
 
 app.use(express.static(__dirname + '/public'));
@@ -31,8 +46,9 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/cap001', (req, res) => {
-    res.render('cap001');
+app.get('/Maou/cap001', (req, res) => {
+    let num = maouNums[0];
+    res.render('Maou/cap001', {num});
 });
 
 /*

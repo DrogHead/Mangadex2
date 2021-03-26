999192b9618bc09a07c7d4ddec4d247c5d58bea5
DrogHead <giuvit003@gmail.com>
2021-03-26 15:20:17 +0100

    almostThere

--- server.js
+++ server.js
@@ -4,86 +4,117 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = 8000;
-const dirPath = path.join(__dirname, 'public/images/Maou');
-
-const maouLinks = [];
-const maouNums = [];
-
-var nMaou = 0;
-fs.readdir(dirPath, (err, files) => {
-    if(err) {
-        return console.log('Error: ' + err);
-    }
-    files.forEach(file => {
-        nMaou++;
-        let numLink = (nMaou < 100 ? (nMaou < 10 ? "00" : "0") : "") + nMaou;
-
-        maouLinks.push("http://127.0.0.1:8000/Maou/cap" + numLink);
-
-        fillNums(path.join(dirPath, 'C'+numLink));
-
-    });
-});
-
-function fillNums(pth) {
-    let n = 0;
-    let files = fs.readdirSync(pth);
-    files.forEach(file => {
-        n++;
-    });
-    maouNums.push(n);
-}
-
-
-
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+const PORT = 8000;
+
+const MAXNUM = 999;
+
+// Database
 const manga = [{
     titolo: 'Maou no Ore ga Dorei Elf',
     img: '/images/Cover/maou.png',
     desc: 'Zagan is feared as an evil mage, he is awkward and has a sharp tongue, and once again had to put down thieves that encroached on his territory when he researching that morning. In a dark auction, he finds a white slave elf, Nephie, who holds a peerless beauty',
-    link: 'Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba Ii? chapter 33',
-    url: '/Maou'
+    url: '/Maou',
+    nick: 'Maou',
+    dirPath: "",
+    chapterLinks: [],
+    numOfPages: []
 },
 {
     titolo: 'Assassin de Aru Ore no Sutetasu ga Yuusha Yori mo akiraka ni tsuyoi nodaga',
     img: '/images/Cover/assassin.png',
     desc: 'Akira is just having an usual day at his second year of high school when he and his fellow classmates get sucked into another world through a portal! This fantasy land is called Morigan, and is a world filled with Demons, Humans, Elves and other mystical creatures.',
-    link: 'My Status as an Assassin Obviously Exceeds the Brave s chapter 22',
-    url: 'Assassin'
+    url: '/Assassin',
+    nick: 'Assassin',
+    dirPath: "",
+    chapterLinks: [],
+    numOfPages: []
 },
 {
     titolo: 'I Am The Sorcerer King',
     img: '/images/Cover/sorcerer.png',
     desc: '10 years ago, the monster horde from the rift formed from space and time started attacking the mankind. At the same time, people have started to awaken the power and began hunting the monsters for fame and money Lee SungHoon, in need of money because of his mother’s sickness, takes a dangerous job to help hunt those monsters four times ',
-    link: 'I Am The Sorcerer King chapter 17',
-    url: 'Sorcerer'
+    url: '/Sorcerer',
+    nick: 'Sorcerer',
+    dirPath: "",
+    chapterLinks: [],
+    numOfPages: []
 }];
 
+
+// Fill directories for manga
+manga.forEach(m => {
+    m.dirPath = path.join(__dirname, 'public/images' + m.url);
+});
+
+// Fill number of chapters and number of pages per chapter
+manga.forEach(m => {
+
+    let n = 0;
+    fs.readdir(m.dirPath, (err, files) => {
+
+        if (err) return console.log('Error: ' + err);
+
+        files.forEach(file => {
+
+            n++;
+            let str = (n < 100 ? (n < 10 ? "00" : "0") : "") + n;
+            m.chapterLinks.push('http://127.0.0.1:' + PORT + m.url + '/cap' + str);
+
+            let nn = 0;
+            let ff = fs.readdirSync(path.join(m.dirPath, 'C' + str));
+            ff.forEach(f => {
+                nn++;
+            })
+            m.numOfPages.push(nn);
+
+        });
+
+    });
+
+});
+
+
+// Homepage
 app.get('/', (req, res) => {
     res.render('index', {
         manga, PORT
     })
 });
 
-app.get('/Maou', (req, res) => {
-    let m = manga[0]
-    res.render('Maou/index', {nMaou, maouLinks, m});
+
+// Manga pages
+manga.forEach(m => {
+
+    app.get(m.url, (req, res) => {
+        let links = m.chapterLinks;
+        let nLinks = m.chapterLinks.length;
+        res.render(m.nick + '/index', { m, links, nLinks });
+    });
+
 });
 
 
-app.get('/Maou/cap001', (req, res) => {
-    let num = maouNums[0];
-    res.render('Maou/cap001', {num});
-});
+// Chapters pages
+for (let i = 1; i <= MAXNUM; i++) {
+    let str = (i < 100 ? (i < 10 ? "00" : "0") : "") + i;
+    manga.forEach(m => {
 
-/*
-app.get('*', (req, res) => {
-    res.render('error');
-})
-*/
+        app.get(m.url+'/cap'+str, (req, res) => {
+            if(i > m.chapterLinks.length) return;
+            else {
+                let num = m.numOfPages[i-1];
+                let index = i;
+                res.render(m.nick+'/cap'+str, {num, index});
+            }
+        });
+
+    });
+}
+
+
 
 const server = app.listen(PORT, () => {
     console.log('Listening at port ' + PORT);
